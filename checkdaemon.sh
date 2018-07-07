6e87ae9ea922fd0e5805a479ed905b2bb93ef395
Crypt0 N1nj4 <38593912+crypt0n1nj4@users.noreply.github.com>
2018-07-07 09:01:56 -0400

    Update checkdaemon.sh

--- checkdaemon.sh
+++ checkdaemon.sh
@@ -2,15 +2,15 @@
 # checkdaemon.sh
 # Make sure the daemon is not stuck.
 # Add the following to the crontab (i.e. crontab -e)
-# */30 * * * * ~/xbrnode/checkdaemon.sh
+# */30 * * * * ~/sendnode/checkdaemon.sh
 
-previousBlock=$(cat ~/xbrnode/blockcount)
-currentBlock=$(bitrewardsd getblockcount)
+previousBlock=$(cat ~/sendnode/blockcount)
+currentBlock=$(sendd getblockcount)
 
-wire-cli getblockcount > ~/wirenode/blockcount
+send-cli getblockcount > ~/wirenode/blockcount
 
 if [ "$previousBlock" == "$currentBlock" ]; then
-  bitrewardsd stop
+  sendd stop
   sleep 10
-  ./bitrewardsd -daemon
+  ./sendd -daemon
 fi
