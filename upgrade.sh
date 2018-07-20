21ab0786eb820c4342cf89aa0a2c9f9e2191f28c
Crypt0 N1nj4 <38593912+crypt0n1nj4@users.noreply.github.com>
2018-07-19 23:01:57 -0400

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,15 +2,15 @@
 # upgrade.sh
 # Make sure sendd is up-to-date
 # Add the following to the crontab (i.e. crontab -e)
-# 0 0 */1 * * ~/sendnode/upgrade.sh
+# 0 0 */1 * * ~/zsub1xnode/upgrade.sh
 
 apt update
 
-if apt list --upgradable | grep -v grep | grep sendd > /dev/null
+if apt list --upgradable | grep -v grep | grep zsub1xd > /dev/null
 then
-  sendd stop && sleep 10
+  zsub1x-cli stop && sleep 10
   rm ~/.send/peers.*
-  apt install sendd -y && ./sendd -daemon &
+  apt install zsub1xd -y && ./zsub1xd -daemon &
 else
   exit
 fi
